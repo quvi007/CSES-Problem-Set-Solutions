06d4738854410bfaff8e23ffd30a550e85310c9c
Quvi <1805007@ugrad.cse.buet.ac.bd>
2021-07-21 11:58:59 +0600

    added soln of Weird Algorithm

---
+++ 1. Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n;
+    cin >> n;
+
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2) n = n * 3 + 1;
+        else n /= 2;
+    }
+
+    cout << 1 << "\n";
+    return 0;
+}
